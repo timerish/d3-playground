d202380985d159aca75c8831bb8dac82611a50ff
littleGauze <810483824@qq.com>
2016-01-05 10:03:31 +0800

    Create expressions.js

---
+++ littleGauze/2016/01/04/expressions.js
@@ -0,0 +1,56 @@
+body.html('');
+title = 'JavaScript Expressions';
+d3.select('title')
+  .html(title);
+
+body.append('h1')
+  .html(title)
+
+body.append('p')
+  .html('An expression is any valid unit of code that resolves to a value.');
+
+output = body.append('div')
+  .style('border', '1px solid #CCC')
+  .style('margin', '0.5em')
+  .style('padding', '0.5em');
+
+next_button = body.append('button')
+  .html('next >')
+  .on('click', next);
+
+expression_list = [
+'1',
+'1 + 1',
+'x',
+'x = 1',
+'x'
+];
+
+function next () {
+  u=setInterval(function(){
+    
+    expression = expression_list.shift();
+    if(expression_list.length <=0) { 
+      next_button.style('display', 'none');
+      clearInterval(u);
+    };
+    output.append('pre')
+      .style('color', 'green')
+      .text('expression > ' + expression);
+    setTimeout(function () {
+      try {
+        value = eval(expression);
+        output.append('pre')
+          .style('color', 'blue')
+          .text('     value > ' + value);
+      }
+      catch (e) {
+        output.append('pre')
+          .style('color', 'red')
+          .text('     error > ' + e);
+      }
+    }, 1000);
+  
+  },3000);
+  
+}
