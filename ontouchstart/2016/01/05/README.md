f94c9c502f4bd971635feb54288035635c8ca8aa
ontouchstart <ontouchstart@gmail.com>
2016-01-05 18:39:49 -0500

    https://bigdata-mindstorms.github.io/d3-playground/#https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/01/05/category10_color_dispatch.js

---
+++ ontouchstart/2016/01/05/README.md
@@ -0,0 +1,3 @@
+# 2016/01/05
+
+- [Category10 Color Dispatch](https://bigdata-mindstorms.github.io/d3-playground/#https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/01/05/category10_color_dispatch.js)
---
+++ ontouchstart/2016/01/05/category10_color_dispatch.js
@@ -0,0 +1,39 @@
+title = 'Category10 Color Dispatch';
+
+body.html('')
+ .style('margin', 0)
+ .style('padding', 0)
+ .style('font-family', 'American Typewriter');
+
+
+d3.select('title')
+  .html(title);
+
+body.append('h1')
+  .style('text-align', 'center')
+  .style('margin', '1em')
+  .html(title);
+
+color_dispatch = d3.dispatch('color');
+color_dispatch.on('color', function (color) {
+  body.style('background-color', color); 
+});
+body.selectAll('div')
+  .data(d3.range(10).map(d3.scale.category10()))
+  .enter()
+  .append('div')
+  .html(function (d) { 
+    return d;
+  })
+  .style('background-color', function (d) {
+    return d;
+  })
+  .style('width', '20%')
+  .style('text-align', 'center')
+  .style('display', 'inline-block')
+  .on('mouseover', function (d) { 
+    color_dispatch.color(d); 
+  })
+  .on('touchstart', function (d) { 
+    color_dispatch.color(d); 
+  });
