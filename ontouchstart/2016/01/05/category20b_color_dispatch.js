8071a2e843e1118e03898f42a2b40e1f5687f82f
ontouchstart <ontouchstart@gmail.com>
2016-01-05 18:45:25 -0500

    https://bigdata-mindstorms.github.io/d3-playground/#https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/01/05/category20b_color_dispatch.js

---
+++ ontouchstart/2016/01/05/category20b_color_dispatch.js
@@ -0,0 +1,40 @@
+title = 'Category20b Color Dispatch';
+
+body.html('')
+ .style('margin', 0)
+ .style('padding', 0)
+ .style('font-family', 'American Typewriter');
+
+
+d3.select('title')
+  .html(title);
+
+body.append('h1')
+  .style('text-align', 'center')
+  .style('margin', '1em')
+  .html(title);
+
+color_dispatch = d3.dispatch('color');
+color_dispatch.on('color', function (color) {
+  body.style('background-color', color); 
+});
+body.selectAll('div')
+  .data(d3.range(20).map(d3.scale.category20b()))
+  .enter()
+  .append('div')
+  .html(function (d) { 
+    return d;
+  })
+  .style('background-color', function (d) {
+    return d;
+  })
+  .style('width', '20%')
+  .style('margin-bottom', '1em')
+  .style('text-align', 'center')
+  .style('display', 'inline-block')
+  .on('mouseover', function (d) { 
+    color_dispatch.color(d); 
+  })
+  .on('touchstart', function (d) { 
+    color_dispatch.color(d); 
+  });
