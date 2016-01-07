bbf9cfc650f47b375012643a1862c9ec243d436f
ontouchstart <ontouchstart@gmail.com>
2016-01-07 18:39:53 -0500

    https://bigdata-mindstorms.github.io/d3-playground/#https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/01/07/yp_random_data_bars.js

---
+++ ontouchstart/2016/01/07/yp_random_data_bars.js
@@ -0,0 +1,34 @@
+function yp(obj) { 
+  try {
+    body.append('pre')
+      .html(js_yaml.dump(obj));
+  }
+  catch (error) {
+    body.append('pre')
+      .style('color', 'red')
+      .html(js_yaml.dump(error));
+  }
+}
+
+function bars(obj) {
+  try {
+   body.append('div').selectAll('div').data(obj.list).enter().append('div')
+    .style('height', '10px')
+    .style('border', '1px solid #ccc')
+    .style('background', 'blue')
+    .style('width', function (d) { return d * 100 + '%'; })
+  }
+  catch (error) {
+    body.append('pre')
+      .style('color', 'red')
+      .html(js_yaml.dump(error));
+  }  
+}
+
+
+data = {};
+data.name = "10 random numbers";
+data.list = d3.range(10).map(Math.random);
+yp(data);
+bars(data);
+
