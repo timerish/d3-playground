808f7381986893fe7edd302258939c5a8a75f8d5
Sam Liu <ontouchstart@gmail.com>
2016-01-07 17:51:13 -0500

    Update yp_data.js

--- ontouchstart/2016/01/07/yp_data.js
+++ ontouchstart/2016/01/07/yp_data.js
@@ -1,6 +1,13 @@
 function yp(obj) { 
-  body.append('pre')
-    .html(js_yaml.dump(obj));
+  try {
+    body.append('pre')
+      .html(js_yaml.dump(obj));
+  }
+  catch (error) {
+    body.append('pre')
+      .style('color', 'red')
+      .html(js_yaml.dump(error));
+  }
 }
 data = {};
 data.name = "A list of 10 numbers";
