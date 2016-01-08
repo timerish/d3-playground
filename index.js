28f4fa65c3f2897050e0e25cd7a2d1889b7903e1
ontouchstart <ontouchstart@gmail.com>
2016-01-08 10:03:17 -0500

    add review/hide buttons

--- index.js
+++ index.js
@@ -8,16 +8,39 @@ body = d3.select('body')
 body.append('h1')
   .html('d3-playground');
 
-run_button = body.append('button')
+review_button = body.append('button')
+  .style('display', 'none')
+  .html('Review the code')
+  .on('click', review);
+
+hide_button = body.append('button')
   .style('display', 'block')
-  .html('Review the code and run')
-  .on('click', run);
+  .html('Hide the code')
+  .on('click', hide);
 
 code = body.append('pre')
   .attr('contenteditable', true)
+  .style('display', 'block')
   .style('white-space', 'pre-wrap')
   .style('padding','1em')
   .style('border','1px solid #CCC');
+
+function review () {
+  review_button.style('display', 'none');
+  hide_button.style('display', 'block');
+  code.style('display', 'block');
+}
+
+function hide () {
+  hide_button.style('display', 'none');
+  review_button.style('display', 'block');
+  code.style('display', 'none');
+}
+
+run_button = body.append('button')
+  .style('display', 'block')
+  .html('Run the code')
+  .on('click', run);
 
 function run () {
   eval(code.text());
