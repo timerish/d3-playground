eedaf21cdb5a2763b32bb5613391272089b54d98
sinlov <sinlovgmppt@gmail.com>
2016-01-18 01:22:13 +0800

    try nom-chance for random anythins

---
+++ sinlov/npm-chance/README.md
@@ -0,0 +1,13 @@
+# d3js use chance
+
+## Information of chance
+
+can use in browser, component, Node...
+
+for Random anything!
+
+[npm-chance](https://www.npmjs.com/package/chance)
+
+## demo
+
+[chance examples](https://bigdata-mindstorms.github.io/d3-playground/sinlov/npm-chance)
---
+++ sinlov/npm-chance/index.js
@@ -0,0 +1,60 @@
+d3 = require('d3');
+marked = require('marked');
+js_yaml = require('js-yaml');
+Chance = require('chance');
+
+body = d3.select('body')
+  .style('padding', '1em');
+
+body.append('h1')
+  .html('d3-playground');
+
+review_button = body.append('button')
+  .style('display', 'none')
+  .html('Review the code')
+  .on('click', review);
+
+hide_button = body.append('button')
+  .style('display', 'block')
+  .html('Hide the code')
+  .on('click', hide);
+
+code = body.append('pre')
+  .attr('contenteditable', true)
+  .style('display', 'block')
+  .style('white-space', 'pre-wrap')
+  .style('padding','1em')
+  .style('border','1px solid #CCC');
+
+function review () {
+  review_button.style('display', 'none');
+  hide_button.style('display', 'block');
+  code.style('display', 'block');
+}
+
+function hide () {
+  hide_button.style('display', 'none');
+  review_button.style('display', 'block');
+  code.style('display', 'none');
+}
+
+run_button = body.append('button')
+  .style('display', 'block')
+  .html('Run the code')
+  .on('click', run);
+
+function run () {
+  eval(code.text());
+}
+
+src = location.hash.replace(/^#/, '') || 'chance.js';
+
+d3.text(src, function (e, d){
+  if(!e) {
+    code.text(d);
+  }
+  else {
+    code.text('// Error: ' + e.statusText);
+    console.log(e);
+  }
+})
