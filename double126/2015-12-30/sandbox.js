7a73df64953d7ab62cbfcab21b867e9c184e34cb
double126 <172127079@qq.com>
2016-01-17 23:51:16 +0800

    Update sandbox.js

--- double126/2015-12-30/sandbox.js
+++ double126/2015-12-30/sandbox.js
@@ -1,1 +1,26 @@
-alert("hello world!");
+//alert("hello world!");
+
+var dataset = [
+              {x : 20, y : 30, r : 10}, 
+              {x : 60, y : 30, r : 10}, 
+              {x : 80, y : 30, r : 10},
+              {x : 100, y : 30, r : 10}
+    ];
+
+var svg = d3.select("body").append("svg");
+
+var circle = svg.selectAll(".myCircle")
+                .data(dataset)
+                .enter()
+                .append("circle")
+                .attr("class", "myCircle")
+                .attr("cx", function(d){
+                  return d.x;
+                })
+                .attr("cy", function(d){
+                  return d.y;
+                })
+                .attr("r", function(d){
+                  return d.r;
+                })
+                .style("fill", "red");
