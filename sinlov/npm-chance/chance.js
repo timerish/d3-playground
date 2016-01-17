c95e7ace3eaebae475ea63266a9fd6c844490edc
sinlov <sinlovgmppt@gmail.com>
2016-01-18 01:28:03 +0800

    update chance data for test

--- sinlov/npm-chance/chance.js
+++ sinlov/npm-chance/chance.js
@@ -1,5 +1,5 @@
 hide();
-var title = 'chance examples';
+var title = 'npm-chance examples';
 d3.select('title').html(title);
 
 var chance = new Chance();
@@ -14,15 +14,15 @@ var examples = [
   "chance.character({casing: 'lower'})",
   "chance.character({symbols: true})",
   "chance.floating()",
-  "chance.floating({fixed: 7})",
+  "chance.floating({fixed: 177})",
   "chance.floating({min: 0, max: 100})",
   "chance.integer()",
-  "chance.integer({min: -20, max: 20})",
+  "chance.integer({min: -40, max: 40})",
   "chance.natural()",
-  "chance.natural({min: 1, max: 20})",
+  "chance.natural({min: 1, max: 50})",
   "chance.string()",
-  "chance.string({length: 5})",
-  "chance.string({pool: 'abcde'})",
+  "chance.string({length: 10})",
+  "chance.string({pool: 'qwert'})",
   "chance.paragraph()",
   "chance.paragraph({sentences: 1})",
   "chance.sentence()",
