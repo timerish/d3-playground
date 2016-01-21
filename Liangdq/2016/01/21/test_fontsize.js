29ef38e29ec53e78fe8a59e1048267cf85f81019
piKapiKa <419910312@qq.com>
2016-01-21 21:37:42 +0800

    Create test_fontsize.js

---
+++ Liangdq/2016/01/21/test_fontsize.js
@@ -0,0 +1,8 @@
+d3.select("body").append("p").html("World Peace").style("color","blue").style("font-size","x-small");
+d3.select("body").append("p").html("World Peace").style("color","blue").style("font-size","small");
+d3.select("body").append("p").html("World Peace").style("color","blue").style("font-size","large");
+d3.select("body").append("p").html("World Peace").style("color","blue").style("font-size","x-large");
+d3.select("body").append("div").append("p").html("World Peace").style("color","blue").style("font-size","x-small");
+d3.select("body").append("div").append("p").html("World Peace").style("color","blue").style("font-size","small");
+d3.select("body").append("div").append("p").html("World Peace").style("color","blue").style("font-size","large");
+d3.select("body").append("div").append("p").html("World Peace").style("color","blue").style("font-size","x-large");
