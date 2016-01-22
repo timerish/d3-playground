649ed3d0fb827923445c8e8f2d95d14b0a09ec5f
Kyle Han <kyhan316@gmail.com>
2016-01-22 06:27:56 -0500

    Add 1 glossary term

--- LivingProgram/2016/01/22/Glossary.js
+++ LivingProgram/2016/01/22/Glossary.js
@@ -1,1 +1,3 @@
-
+d3.select('body').append('dl').append('dt').html("Github").append('dd').html("is a web-based Git repository hosting service")
+//.append('dt').html("Gist").append('dd').html("is the substance or essence of a speech or text")
+//the commented text does not allow me to create another term with another definition, could someone help me?
