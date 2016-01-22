7621d00136c83a235a83ca8510f146c081d99905
Kyle Han <kyhan316@gmail.com>
2016-01-22 06:45:53 -0500

    Added 4 font-sizes with defined px size

--- LivingProgram/2016/01/22/fontSizePx.js
+++ LivingProgram/2016/01/22/fontSizePx.js
@@ -1,1 +1,4 @@
-
+d3.select('body').append('div').append('p').html("Github").style('font-size', '4px');
+d3.select('body').append('div').append('p').html("Github").style('font-size', '16px');
+d3.select('body').append('div').append('p').html("Github").style('font-size', '64px');
+d3.select('body').append('div').append('p').html("Github").style('font-size', '256px');
