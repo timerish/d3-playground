403980edc915d4f14b2f4234bf3e3b0c9b04bff9
Kyle Han <kyhan316@gmail.com>
2016-01-22 06:41:45 -0500

    Added 7 font-sizes

--- LivingProgram/2016/01/22/fontSize.js
+++ LivingProgram/2016/01/22/fontSize.js
@@ -1,1 +1,7 @@
-
+d3.select('body').append('div').append('p').html("Github").style('font-size', 'xx-small');
+d3.select('body').append('div').append('p').html("Github").style('font-size', 'x-small');
+d3.select('body').append('div').append('p').html("Github").style('font-size', 'small');
+d3.select('body').append('div').append('p').html("Github").style('font-size', 'medium');
+d3.select('body').append('div').append('p').html("Github").style('font-size', 'large');
+d3.select('body').append('div').append('p').html("Github").style('font-size', 'x-large');
+d3.select('body').append('div').append('p').html("Github").style('font-size', 'xx-large');
