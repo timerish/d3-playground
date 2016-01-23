5a9495b7194805d81fa5e1a43be69f173d50e8d1
Sam Liu <ontouchstart@gmail.com>
2016-01-23 12:26:03 -0500

    丙正正 鼠标变小手

--- ontouchstart/2016/01/23/随笔.js
+++ ontouchstart/2016/01/23/随笔.js
@@ -4,3 +4,4 @@ d3.select('title').html(题目);
 大标题.html(题目);
 第一段 = d3.select('body').append('div').html('这次试图能用中文的尽量用中文，但是又不陷入<span id="丙正正">丙正正</span>的误区。');
 丙正正 = d3.select('span#丙正正').style('background','yellow');
+丙正正.style('cursor', 'pointer');
