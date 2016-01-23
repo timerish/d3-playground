6a34e3db5b2cef7bd780a8c2fb8419b1dfbb0727
ontouchstart <ontouchstart@gmail.com>
2016-01-23 14:23:19 -0500

    buttons inline-block

--- index.js
+++ index.js
@@ -8,39 +8,40 @@ body = d3.select('body')
 body.append('h1')
   .html('d3-playground');
 
+run_button = body.append('button')
+  .style('display', 'inline-block')
+  .style('margin-right', '1em')
+  .html('Run 运行')
+  .on('click', run);
+
 see_button = body.append('button')
   .style('display', 'none')
   .html('See the code 看代码')
   .on('click', see);
 
 hide_button = body.append('button')
-  .style('display', 'block')
+  .style('display', 'inline-block')
   .html('Hide the code 藏代码')
   .on('click', hide);
 
 code = body.append('pre')
   .attr('contenteditable', true)
-  .style('display', 'block')
+  .style('display', 'inline-block')
   .style('white-space', 'pre-wrap')
   .style('padding','1em')
   .style('border','1px solid #CCC');
 
 function see () {
   see_button.style('display', 'none');
-  hide_button.style('display', 'block');
+  hide_button.style('display', 'inline-block');
   code.style('display', 'block');
 }
 
 function hide () {
   hide_button.style('display', 'none');
-  see_button.style('display', 'block');
+  see_button.style('display', 'inline-block');
   code.style('display', 'none');
 }
-
-run_button = body.append('button')
-  .style('display', 'block')
-  .html('Run 运行')
-  .on('click', run);
 
 function run () {
   eval(code.text());
