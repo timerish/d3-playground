00f174735e8e5e78e116d81b02db6004a774a8fb
Sam Liu <ontouchstart@gmail.com>
2016-01-23 13:45:17 -0500

    mindstorms_cover

--- ontouchstart/2016/01/23/随笔.js
+++ ontouchstart/2016/01/23/随笔.js
@@ -9,3 +9,7 @@ d3.select('title').html(题目);
   location = 'http://baike.baidu.com/client/view/1718420.htm';
 });
 mindstorms = d3.select('body').append('h1').html('Mindstorms (头脑风暴)');
+mindstorms_cover = d3.select('body').append('img').attr({
+  src: 'https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/01/23/mindstorms_cover.png',
+  width: '50%'
+})
