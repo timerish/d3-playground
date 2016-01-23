4635dc73754f3f9724472c1cf6bfb4ca3dc73f5a
Sam Liu <ontouchstart@gmail.com>
2016-01-23 12:28:55 -0500

    丙正正 加上链接到百度文章

--- ontouchstart/2016/01/23/随笔.js
+++ ontouchstart/2016/01/23/随笔.js
@@ -5,3 +5,6 @@ d3.select('title').html(题目);
 第一段 = d3.select('body').append('div').html('这次试图能用中文的尽量用中文，但是又不陷入<span id="丙正正">丙正正</span>的误区。');
 丙正正 = d3.select('span#丙正正').style('background','yellow');
 丙正正.style('cursor', 'pointer');
+丙正正.on('click', function () {
+  location = 'http://baike.baidu.com/client/view/1718420.htm';
+});
