8d9b8de5352262e58d5d0152e157f70233b8043a
Sam Liu <ontouchstart@gmail.com>
2016-01-24 08:27:20 -0500

    call add_link()

--- ontouchstart/2016/01/24/函数.js
+++ ontouchstart/2016/01/24/函数.js
@@ -3,3 +3,6 @@ d3.select('title').html(title);
 function add_link(selection, href, html) {
   selection.append('a').attr('href', href).html(html)
 }
+
+add_link(d3.select('body').append('p').html('Here is the link '), 'https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Functions', 'MDN Functions');
+add_link(d3.select('body').append('p').html('这是中文链接 '), 'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Guide/Functions', 'MDN 函数');
