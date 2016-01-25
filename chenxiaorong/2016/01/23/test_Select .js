2d84339b31151560a232c446000c98b651f61dfe
xiaoer <xiaoer_2013@sina.cn>
2016-01-25 16:35:44 +0800

    Update test_Select .js

--- chenxiaorong/2016/01/23/test_Select .js
+++ chenxiaorong/2016/01/23/test_Select .js
@@ -1,4 +1,4 @@
 console.log(d3.select("body"));
-d3.select("body").append("div").html("今天下了好大的雪，好高兴呢，第四次看见雪！！");
-d3.select("body").append("div").attr("id","div_Snow").html("下雪很好，可是好冷（~ . ~）");
+d3.select("body").append("div").html("今天下了好大的雪，好高兴呢，第四次看见雪！！").style("color","red");
+d3.select("body").append("div").attr("id","div_Snow").html("下雪很好，可是好冷（~ . ~）").style("color","blue");
 console.log(d3.select("div#div_Snow").text());
