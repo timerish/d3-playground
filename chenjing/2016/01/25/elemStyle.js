21d93f179b4cda6550cab7ab5f5cded19c623860
JingCHEN <dianachenfr@gmail.com>
2016-01-26 11:29:08 +0800

    element style

---
+++ chenjing/2016/01/25/elemStyle.js
@@ -0,0 +1,8 @@
+d3.select('body').append('div').append('p').html("It's going to snow this weekend! ⛄️")
+  .style('font-size', 'x-small').style("color","red");
+d3.select('body').append('div').append('p').html("It's going to snow this weekend! ⛄️")
+  .style('font-size', 'small').style("color","blue");
+d3.select('body').append('div').append('p').html("It's going to snow this weekend! ⛄️")
+  .style('font-size', 'large').style("color","green");
+d3.select('body').append('div').append('p').html("It's going to snow this weekend! ⛄️")
+  .style('font-size', 'x-large').style("color","yellow");
