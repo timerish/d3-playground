2c5c328e73b47a491655404facd79939f8c88508
aaron-suen <sunzhaochun@126.com>
2016-01-26 16:45:40 +0800

    Update selector.js

--- aaron-suen/2016/01/26/selector.js
+++ aaron-suen/2016/01/26/selector.js
@@ -1,4 +1,4 @@
 console.log(d3.select("body"));
 d3.select("body").append("p").html("Hey siri, 上海田子坊在哪里？");
 d3.select("body").append("p").attr("id","p_answer").html("北京之南，南京之东");
-console.log(d3.select("p#p_answer").style("color","green").text());
+console.log(d3.selectAll("p#p_answer").style("color","green").text());
