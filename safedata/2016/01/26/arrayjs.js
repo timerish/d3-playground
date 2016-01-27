3521feaed4babdfc9e7a9da073f35f86a159a7d0
safedata <data@cloud-link.net>
2016-01-27 00:13:32 -0500

    Update arrayjs.js

--- safedata/2016/01/26/arrayjs.js
+++ safedata/2016/01/26/arrayjs.js
@@ -7,10 +7,10 @@ d3.select('body').append('h1').html(title);
 function print_array(arrayname, array) {
   d3.select('body').append('div').html(arrayname);
   var ul = d3.select('body').append('ul');
-  array.forEach(function(item, i) {
-    ul.append('li').html(item + '  ' + i);
+  array.forEach(function(item, index) {
+    ul.append('li').html(item + ' ' + index);
   });
 }
-
+arrayname = ' my array ';
 Test_array = ['elem1',1,{}]
-print_array(Test_array);
+print_array(arrayname, Test_array);
