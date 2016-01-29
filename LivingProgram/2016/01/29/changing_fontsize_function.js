44d29c6216c681761810d403844c415313c61701
Kyle Han <kyhan316@gmail.com>
2016-01-29 06:40:24 -0500

    Added a fontsize changing function

--- LivingProgram/2016/01/29/changing_fontsize_function.js
+++ LivingProgram/2016/01/29/changing_fontsize_function.js
@@ -1,1 +1,9 @@
+d3.select('body').append('div').html('test');
 
+function change_fontsize(selection, pixel_size) {
+  selection.style('font-size', pixel_size);
+  //a function that changes the fontsize
+}
+
+change_fontsize(d3.select('body').append('p').html('I am smaller now'), '4px');
+change_fontsize(d3.select('body').append('p').html('I am Bigger now'), '100px');
