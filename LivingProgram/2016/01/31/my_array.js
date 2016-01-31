4f05c3ce8a99c4b93afc8516b8081e809e05a38a
Kyle Han <kyhan316@gmail.com>
2016-01-31 11:04:54 -0500

    Added a very standard array

--- LivingProgram/2016/01/31/my_array.js
+++ LivingProgram/2016/01/31/my_array.js
@@ -1,1 +1,14 @@
+title = 'Shopping List';
+d3.select('title').html(title);
+d3.select('body').append('h1').html(title);
 
+function print_array(array) {
+  var ol = d3.select('body').append('ol');
+  array.forEach(function(item, index, array) {
+    ol.append('li').html(item);
+    console.log(item, index);
+  });
+}
+
+shopping_list = ["eggs", "milk", "cheese", "bacon"];
+print_array(shopping_list)
