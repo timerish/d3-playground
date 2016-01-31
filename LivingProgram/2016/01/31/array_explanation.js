931056fd3470f67f583d0594cdd1e044a22040d7
Kyle Han <kyhan316@gmail.com>
2016-01-31 10:45:07 -0500

    Commented on ontouchstart's example

--- LivingProgram/2016/01/31/array_explanation.js
+++ LivingProgram/2016/01/31/array_explanation.js
@@ -1,17 +1,22 @@
 title = '数组';
 d3.select('title').html(title);
-d3.select('body').append('h1').html(title);
+d3.select('body').append('h1').html(title); //gives the document a title
 
 function print_array(array) {
-  var ul = d3.select('body').append('ul');
+  //this function prints the array not only in the console but the body
+  var ul = d3.select('body').append('ul'); //creates unordered list under the variable ul
+  
   array.forEach(function(item, index, array) {
-    ul.append('li').html(item).attr('class', 'item-' + index);
-    console.log(array);
+    //for each array 
+    ul.append('li') //creates data point in the unordered list
+    .html(item)     //items are like 1, 2 ,3 or '香蕉','苹果','大鸭梨' (so these are printed in html)
+    .attr('class', 'item-' + index); 
+    console.log(array); //logs the array in the console (so you can see it)
   });
 }
 
 数组 = [1, 2, 3];
-print_array(数组);
+print_array(数组); //prints an array
 
 水果 = ['香蕉','苹果','大鸭梨'];
-print_array(水果);
+print_array(水果); //prints an array
