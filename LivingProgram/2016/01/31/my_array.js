33d916444ef627dff0f90678df38775011313f67
Kyle Han <kyhan316@gmail.com>
2016-01-31 11:05:13 -0500

    Added number of items in list

--- LivingProgram/2016/01/31/my_array.js
+++ LivingProgram/2016/01/31/my_array.js
@@ -4,6 +4,8 @@ d3.select('body').append('h1').html(title);
 
 function print_array(array) {
   var ol = d3.select('body').append('ol');
+  var array_length = array.length
+  d3.select('body').append('p').html('Number of Items: ' + array_length);
   array.forEach(function(item, index, array) {
     ol.append('li').html(item);
     console.log(item, index);
