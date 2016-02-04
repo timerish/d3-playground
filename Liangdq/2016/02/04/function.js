7d70f1967c0b1d781020a5b2dbb4454e60dd9603
piKapiKa <419910312@qq.com>
2016-02-04 14:46:16 +0800

    Create function.js

---
+++ Liangdq/2016/02/04/function.js
@@ -0,0 +1,10 @@
+selection = d3.select("body").append("div");
+selection.append("input").attr("type","number").attr("id","i_a");
+selection.append("text").html("+");
+selection.append("input").attr("type","number").attr("id","i_b");
+selection.append("button").html("=").on("click",function(){
+var num1 = selection.select("input#i_a").property("value");
+var num2 = selection.select("input#i_b").property("value");
+var sum = num1 + num2;
+selection.append("text").html(sum);
+});
