6563828a065616fb2475ef58511ba96f6a543209
sweetcan <329390143@qq.com>
2016-02-04 14:55:37 +0800

    Create elementstyle.js

---
+++ sweetcan/2016/02/04/20/elementstyle.js
@@ -0,0 +1,8 @@
+d3.select('body')
+  .append('div').html('<p>hello world</p>')
+  .append('a')
+  .attr('href', 'https://www.baidu.com')
+  .html('百度')
+  .style('text-decoration', 'none')
+  .style('font-size','bold')
+  .style('color', 'red');
