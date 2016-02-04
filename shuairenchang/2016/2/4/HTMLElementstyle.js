6d59c3b9e2e5d55e40263ca7f04fa8a70668ec3e
shuairenchang <1164151045@qq.com>
2016-02-04 21:02:08 +0800

    Create HTMLElementstyle.js

---
+++ shuairenchang/2016/2/4/HTMLElementstyle.js
@@ -0,0 +1,4 @@
+d3.select('body').append('div').append('p').html('GOOD GOOD STUDY,DAY DAY UP').style('font-size','x-samll').style('color','red');
+d3.select('body').append('div').append('p').html('GOOD GOOD STUDY,DAY DAY UP').style('font-size','samll').style('color','red');
+d3.select('body').append('div').append('p').html('GOOD GOOD STUDY,DAY DAY UP').style('font-size','large').style('color','red');
+d3.select('body').append('div').append('p').html('GOOD GOOD STUDY,DAY DAY UP').style('font-size','x-large').style('color','red');
