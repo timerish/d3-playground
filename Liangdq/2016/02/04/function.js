b8605952347c7f3cfff2a6b081fcbf3ba558c0c7
piKapiKa <419910312@qq.com>
2016-02-04 15:33:19 +0800

    Update function.js

--- Liangdq/2016/02/04/function.js
+++ Liangdq/2016/02/04/function.js
@@ -5,7 +5,6 @@ selection.append("input").attr("type","number").attr("id","i_b");
 selection.append("button").html("=").on("click",function(){
 var num1 = selection.select("input#i_a").property("value");
 var num2 = selection.select("input#i_b").property("value");
-var sum = null;
-sum = Number.parseFloat(num1) + Number.parseFloat(num2);
+var sum = Number.parseFloat(num1) + Number.parseFloat(num2);
 selection.append("text").html(sum);
 });
