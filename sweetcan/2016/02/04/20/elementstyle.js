f92eecc1d97dd8b86bdd697e8cdfde95dd76fc48
sweetcan <329390143@qq.com>
2016-02-04 14:53:19 +0800

    Delete elementstyle.js

--- sweetcan/2016/02/04/20/elementstyle.js
+++
@@ -1,8 +0,0 @@
-d3.select('body')
-  .append('div').html('<p>hello world</p>')
-  .append('a')
-  .attr('href', 'https://www.baidu.com')
-  .html('百度')
-  .style('text-decoration', 'none')
-  .style('font-size','bold')
-  .style('color', 'red');
