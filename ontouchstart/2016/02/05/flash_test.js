93235dfaea331d5f8c239441da47a801167fd936
ontouchstart <ontouchstart@gmail.com>
2016-02-05 15:31:42 -0500

    add data block to flash_test.js

--- ontouchstart/2016/02/05/flash_test.js
+++ ontouchstart/2016/02/05/flash_test.js
@@ -1,8 +1,17 @@
+---
+data :
+  object :
+    attr : 
+      width : 600,
+      height : 600
+  embed:
+    attr:
+      width : 600
+      height : 600
+      src : '/d3-playground/ontouchstart/2016/02/05/test.swf'
+---
 d3.select('body')
   .append('object')
-  .attr({width: 600, height: 600})
+  .attr({{ page.data.object.attr | jsonify }})
   .append('embed')
-  .attr({
-    width: 600, 
-    height: 600, 
-    src : '/d3-playground/ontouchstart/2016/02/05/test.swf'});
+  .attr({{ page.data.embed.attr | jsonify }});
