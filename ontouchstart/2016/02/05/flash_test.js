c6f3a0ab287675b923c1b1abf51f333624748725
ontouchstart <ontouchstart@gmail.com>
2016-02-05 10:17:07 -0500

    local test.swf

--- ontouchstart/2016/02/05/flash_test.js
+++ ontouchstart/2016/02/05/flash_test.js
@@ -1,1 +1,8 @@
-d3.select('body').append('object').attr({width: 600, height: 600}).append('embed').attr({width: 600, height: 600, src : 'http://www.adobe.com/content/dotcom/en/devnet/actionscript/samples/drawing_5/_jcr_content/articlecontentAdobe/generic/file.res/5_3Drotation%5b1%5d.swf'})
+d3.select('body')
+  .append('object')
+  .attr({width: 600, height: 600})
+  .append('embed')
+  .attr({
+    width: 600, 
+    height: 600, 
+    src : '/d3-playground/ontouchstart/2016/02/05/test.swf'});
