565cdb2bbe8851ff6c548451827aba7225d6dfbd
Sam Liu <ontouchstart@gmail.com>
2016-02-04 22:11:52 -0500

    Update 恭喜恭喜3d.js

--- ontouchstart/2016/02/04/恭喜恭喜3d.js
+++ ontouchstart/2016/02/04/恭喜恭喜3d.js
@@ -1,7 +1,7 @@
-d3.select('title').html('恭喜恭喜(李宗胜)');
+d3.select('title').html('恭喜恭喜(李宗胜) 3D');
 src = "http://images.apple.com/media/cn/watch/2016/4d377b9e-3d20-434d-adfd-8da768d35426/tv-spot/newyear-song-foryou/watch-newyear-song-foryou-cn-20160122_1280x720h.mp4";
 d3.select('body')
-  .html('<h1>恭喜恭喜(李宗胜)</h1>')
+  .html('<h1>恭喜恭喜(李宗胜) 3D</h1>')
   .append('video')
   .attr({ 
     controls:true,
