1b9805f782effab433dc93fc28d8b5683ad6f8e8
ontouchstart <ontouchstart@gmail.com>
2016-02-09 15:51:27 -0500

    add_drag_out_line

--- ontouchstart/2016/02/09/add_drag_out_line.js
+++ ontouchstart/2016/02/09/add_drag_out_line.js
@@ -28,9 +28,13 @@ drag_out.on("dragstart", function() {
   d3.event.sourceEvent.stopPropagation(); // silence other listeners
 });
 drag_out.on('drag', function (d) {
-  d.x = d3.event.x;
-  d.y = d3.event.y;
-  console.log(d);
+  d3.select(this)
+    .style({ cursor: 'pointer' })
+    .select('line')
+    .attr({
+      x2: function (d) { return window.innerWidth / 2  + d3.event.x - d.x },
+      y2: function (d) { return window.innerWidth / 2 + d3.event.y - d.y + 100}
+  });
 });
 
 
@@ -39,22 +43,21 @@ function add_drag_out_line(selection) {
   var rect = selection.node().getBoundingClientRect();
   selection.datum({x: rect.left, y: rect.top});
   selection.call(drag_out);
-  selection.svg = selection.append('svg').style({ 
+  var svg = selection.append('svg').style({ 
     position: 'absolute',
     left: - window.innerWidth / 2,
     top: - window.innerHeight / 2,
     width: window.innerWidth,
-    height: window.innerHeight, 
-    border: '1px solid #ccc',
+    height: window.innerHeight,
     'z-index': -100
   });
 
-  selection.line = selection.svg.append('line')
+  var line = svg.append('line')
     .datum({x: rect.left, y: rect.top})
     .attr({
       x1: window.innerWidth / 2,
       y1: window.innerHeight / 2 + 50,
-      x2: window.innerWidth / 2 + 50,
+      x2: window.innerWidth / 2,
       y2: window.innerHeight / 2 + 50,
       stroke: 'red'
    }); 
