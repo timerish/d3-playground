a1dd0414ffea28debaef31b08052915c01cbe126
Sam Liu <ontouchstart@gmail.com>
2016-02-09 11:56:13 -0500

    Create draggable.js

---
+++ ontouchstart/2016/02/09/draggable.js
@@ -0,0 +1,35 @@
+drag = d3.behavior.drag()
+drag.origin(function(d) { return d; });
+dragmove = function (d) {
+  d.x = d3.event.x;
+  d.y = d3.event.y;
+  d3.select(this)
+    .style({
+      left: function (d) { return d.x },
+      top: function (d) { return d.y },
+    });
+};
+
+drag.on('drag', dragmove);
+
+function draggable(selection) {
+  var rect = selection.node().getBoundingClientRect();
+  selection.datum({x: rect.left, y: rect.top});
+  selection.style({
+    position: 'absolute',
+    left: function (d) { return d.x },
+    top: function (d) { return d.y },
+ });
+
+  selection.call(drag);
+};
+
+selection = d3.select('body')
+  .append('div')
+  .style({
+    width: 100,
+    height: 100,
+    background: 'blue' 
+  });
+  
+draggable(selection)
